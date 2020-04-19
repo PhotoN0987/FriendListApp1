fab7e5af73e77cdd6bc25f964865e9da6ed4067d
marcy <usmarcy0904@gmail.com>
2020-04-19 16:23:41 +0900

    はじめちゃんFWマージ

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { ListView } from './views/ListView'
 import { RegistView } from './views/RegistView'
 
 // axios default values
-axios.defaults.baseURL = 'https://firestore.googleapis.com/v1/projects/YOUR_PROJECT_ID/databases/(default)/documents/'
+axios.defaults.baseURL = 'https://firestore.googleapis.com/v1/projects/friend-list-app-7de6d/databases/(default)/documents/'
 
 // Views
 new ListView().loadView()
