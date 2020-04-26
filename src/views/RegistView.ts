bafe01031aa7d9c745183afc78c00ecb51c731f9
assassin_box <assassin543@outlook.jp>
2020-04-26 17:16:57 +0900

    データベースにユーザー情報追加

--- src/views/RegistView.ts
+++ src/views/RegistView.ts
@@ -1,5 +1,60 @@
 import axios from 'axios'
 
 export class RegistView {
-  public loadView() { }
+
+  //#region Components
+  userName: HTMLInputElement | null;
+  favorite: HTMLInputElement | null;
+  birthday: HTMLInputElement | null;
+  registButton: HTMLElement | null;
+  //#endregion
+
+  constructor() {
+    // HTMLElement取得
+    this.userName = <HTMLInputElement>document.getElementById('user-name')
+    this.favorite = <HTMLInputElement>document.getElementById('favorite')
+    this.birthday = <HTMLInputElement>document.getElementById('birthday')
+    this.registButton = document.getElementById('regist-button')
+
+    // イベントの登録
+    this.registButton?.addEventListener("click", () => {
+      this.registButtonClick()
+    });
+  }
+
+  // buttonがクリックされたとき
+  public registButtonClick() {
+    // 値表示
+    alert(`user-name:${this.userName?.value}\nfavorite:${this.favorite?.value}\nbirthday:${this.birthday?.value}`)
+
+    //データベースに追加
+    axios.post('users', {
+    fields: {
+        name: {
+          stringValue: this.userName?.value
+        },
+        favorite: {
+          stringValue: this.favorite?.value
+        }
+      }
+    })
+
+    // 追加処理
+    // axios.post('users', {
+    //   fields: {
+    //     name: {
+    //       stringValue: 'Foo'
+    //     },
+    //     favorite: {
+    //       stringValue: 'Bar'
+    //     }
+    //   }
+    // })
+    //   .then(response => {
+    //     console.log(response)
+    //   })
+    //   .catch(error => {
+    //     console.log(error)
+    //   })
+  }
 }
